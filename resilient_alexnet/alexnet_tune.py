23fdbf5d60e370c60a3e922cf2905a079ae0e7f3
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-01-28 12:03:08 -0800

    Update alexnet_tune.py

--- resilient_alexnet/alexnet_tune.py
+++ resilient_alexnet/alexnet_tune.py
@@ -65,8 +65,12 @@ def model_attack(model, model_type, attack_type, config, num_classes=NUM_CLASSES
             f = open('/lus/theta-fs0/projects/CVD-Mol-AI/mzvyagin/alexnet_datasets/fashion_splits.pkl', 'rb')
             data = pickle.load(f)
             (x_train, y_train), (x_val, y_val), (x_test, y_test) = data
-            data = DataLoader(Fashion_NP_Dataset(x_test.astype(np.float32), y_test.astype(np.float32)),
-                          batch_size=int(config['batch_size']), shuffle=False)
+            if attack_type != "pgd":
+                data = DataLoader(Fashion_NP_Dataset(x_test.astype(np.float32), y_test.astype(np.float32)),
+                              batch_size=int(config['batch_size']), shuffle=False)
+            else:
+                data = DataLoader(Fashion_NP_Dataset(x_test.astype(np.float32), y_test.astype(np.int64)),
+                                  batch_size=int(config['batch_size']), shuffle=False)
         elif MODEL_TYPE == "caltech":
             f = open('/lus/theta-fs0/projects/CVD-Mol-AI/mzvyagin/alexnet_datasets/caltech_splits.pkl', 'rb')
             data = pickle.load(f)
@@ -82,7 +86,7 @@ def model_attack(model, model_type, attack_type, config, num_classes=NUM_CLASSES
         images, labels = [], []
         for sample in data:
             images.append(sample[0].to(device))
-            labels.append(sample[1].long().to(device))
+            labels.append(sample[1].to(device))
         # images, labels = (torch.from_numpy(images).to(device), torch.from_numpy(labels).to(device))
     elif model_type == "tf":
         fmodel = fb.models.TensorFlowModel(model, bounds=(0, 1))
@@ -90,7 +94,11 @@ def model_attack(model, model_type, attack_type, config, num_classes=NUM_CLASSES
             f = open('/lus/theta-fs0/projects/CVD-Mol-AI/mzvyagin/alexnet_datasets/fashion_splits.pkl', 'rb')
             data = pickle.load(f)
             (x_train, y_train), (x_val, y_val), (x_test, y_test) = data
-            data = tf.data.Dataset.from_tensor_slices((x_test.astype(np.float32), y_test.astype(np.float32))).batch(config['batch_size'])
+            if attack_type != "pgd":
+                data = tf.data.Dataset.from_tensor_slices((x_test.astype(np.float32), y_test.astype(np.float32))).batch(config['batch_size'])
+            else:
+                data = tf.data.Dataset.from_tensor_slices((x_test.astype(np.float32), y_test.astype(np.int64))).batch(
+                    config['batch_size'])
         elif MODEL_TYPE == "caltech":
             f = open('/lus/theta-fs0/projects/CVD-Mol-AI/mzvyagin/alexnet_datasets/caltech_splits.pkl', 'rb')
             data = pickle.load(f)
@@ -105,7 +113,7 @@ def model_attack(model, model_type, attack_type, config, num_classes=NUM_CLASSES
         images, labels = [], []
         for sample in data:
             images.append(sample[0])
-            labels.append(sample[1].astype(np.int64))
+            labels.append(sample[1])
     else:
         print("Incorrect model type in model attack. Please try again. Must be either PyTorch or TensorFlow.")
         sys.exit()
